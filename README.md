3c78217c54300ce5648a181c3719ba4535c9ffd0
f4r4z <farazmh1999@gmail.com>
2019-11-02 18:36:48 -0400

    Init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# PremierLeagueMatchResult
-A final time predictor of a premiere league match based on the data obtained at half time.
+# Premier League Final Result Predictor
+
+This program predicts the final result of a premier league match based on the data given at half time. The program has above 60% accuracy and on average it results in about 65% accuracy.
+The final result is in the form of classification with 3 values, 0, 1, and 2 which mean home win, draw, and away win respectively.
+
+### Algorithm
+I used neural network in order to implement this program. Different step size, hidden layers, and neurons were tested to increase accuracy.
+
+### Libraries
+I used [sklearn](https://scikit-learn.org/stable/) and [pandas](https://pandas.pydata.org/) to implement this program.
+
+### Data
+The data is obtained from [football-data.co.uk](https://www.football-data.co.uk/data.php).
---
+++ nn.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+import glob
+
+# file to data frame and concatenation
+all_files = glob.glob('./data' + '/*.csv')
+df = pd.concat(map(pd.read_csv, all_files), sort=True)
+
+# encode strings to int
+enc = LabelEncoder()
+enc.fit(df['FTR'])
+df['FTR'] = enc.transform(df['FTR'])
+enc.fit(df['HTR'])
+df['HTR'] = enc.transform(df['HTR'])
+
+# x and y
+x = df[['HTHG', 'HTAG', 'HTR', 'HS', 'AS', 'HST', 'AST', 'HF', 'AF', 'HC', 'AC', 'HY', 'AY', 'HR', 'AR', 'B365H', 'B365D', 'B365A']]
+y = df['FTR']
+
+
+# divide data set
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.33)
+
+# neural network: stochastic gradient, step size, layers and neurons, randomized state
+clf = MLPClassifier(solver='lbfgs', alpha=0.0000001,  hidden_layer_sizes=(10, 10))  #previous: random_state=1
+
+# fit
+clf.fit(x_train, y_train)
+
+# predict test data
+yhat = clf.predict(x_test)
+
+# accuracy calculation
+total = 0
+correct = 0
+
+list = y_test.values
+
+for i in range(125):
+    total += 1
+    if yhat[i] == list[i]:
+        correct += 1
+
+# print(list.tolist())
+# print(yhat.tolist())
+
+print(correct / total)
+# original_headers = list(df.columns.values)
+
